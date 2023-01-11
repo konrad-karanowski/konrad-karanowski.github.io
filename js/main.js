b4b606adc9f11ad95ac816fa37b88ed8e35185ad
Konrad Karanowski <konrad.karanowski@gmail.com>
2023-01-11 13:45:57 +0100

    Init commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger')
+    const nav = document.querySelector('.nav-links')
+    const navLinks = document.querySelectorAll('.nav-links li');
+    // toggle nav
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        // animate links
+        navLinks.forEach((link, i) => {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${i / 7 + 0.5}s`
+        });
+
+        // change burger to X
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
